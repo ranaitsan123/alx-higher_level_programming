9b33247276af0a4b64be82538215d273294a26dc
AICHA LAHNITE <halaicha300@gmail.com>
2023-06-13 23:15:49 +0100

    add1

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,83 +1,45 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
-
 /**
-  * is_palindrome - Checks if a singly linked list is a palindrome
-  * @head: The head of the singly linked list
-  *
-  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
-  */
+ * is_palindrome - function that check for palindrom
+ * @head:list
+ *Return: 0 or 1
+ */
 int is_palindrome(listint_t **head)
 {
-    listint_t *start = NULL, *end = NULL;
-    unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
+	const listint_t *current;
+	int len;
+	int i, j;
+	int arr[10000];
 
-    if (head == NULL)
-        return (0);
-
-    if (*head == NULL)
-        return (1);
-    
-    start = *head;
-    len = listint_len(start);
-    len_cyc = len * 2;
-    len_list = len_cyc - 2;
-    end = *head;
-
-    for (; i < len_cyc; i = i + 2)
-    {
-        if (start[i].n != end[len_list].n)
-            return (0);
-
-        len_list = len_list - 2;
-    }
-
-    return (1);
-}
-
-/**
-  * get_nodeint_at_index - Gets a node from a linked list
-  * @head: The head of the linked list
-  * @index: The index to find in the linked list
-  *
-  * Return: The specific node of the linked list
-  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-{
-	listint_t *current = head;
-	unsigned int iter_times = 0;
-
-	if (head)
+	if (*head == NULL)
+		return (1);
+	current = *head;
+	len = 0;
+	while (current != NULL)
 	{
-		while (current != NULL)
-		{
-			if (iter_times == index)
-				return (current);
-
-			current = current->next;
-			++iter_times;
-		}
+		current = current->next;
+		len++;
 	}
+	if (len == 1)
+		return (1);
 
-	return (NULL);
-}
-
-/**
-  * slistint_len - Counts the number of elements in a linked list
-  * @h: The linked list to count
-  *
-  * Return: Number of elements in the linked list
-  */
-size_t listint_len(const listint_t *h)
-{
-	int lenght = 0;
-
-	while (h != NULL)
+	current = *head;
+	i = 0;
+	while (current != NULL)
 	{
-		++lenght;
-		h = h->next;
+		arr[i] = current->n;
+		i++;
+		current = current->next;
 	}
-
-	return (lenght);
+	j = 0;
+	i--;
+	len--;
+	while (i >= (len / 2))
+	{
+		if (arr[i] != arr[j])
+			return (0);
+		i--;
+		j++;
+	}
+	return (1);
 }
