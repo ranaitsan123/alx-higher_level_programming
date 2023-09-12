7d2046e23ec21a5258085138816b589d020d7f57
aicha lahnite <halaicha300@gmail.com>
2023-09-12 17:23:29 +0000

    add tasks

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# AICHA LAHNITE <halaicha300@gmail.com>
+"""Defines a text file line-counting function."""
+
+
+def number_of_lines(filename=""):
+    """Return the number of lines in a text file."""
+    lines = 0
+    with open(filename) as f:
+        for line in f:
+            lines += 1
+    return lines
