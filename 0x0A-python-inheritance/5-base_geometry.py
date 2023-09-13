947f2b56e7d27f0a92c20d068c55c00420c70dcf
aicha lahnite <halaicha300@gmail.com>
2023-09-13 20:19:50 +0000

    add tasks

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# AICHA LAHNITE <halaicha300@gmail.com>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
