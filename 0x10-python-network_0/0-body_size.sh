a4a6bd07cd84723b8f705a2f1b651008bdbf382a
ranaitsan123 <95276621+ranaitsan123@users.noreply.github.com>
2024-07-04 22:49:36 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
+
+# Check if a URL argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a GET request using curl, discard headers, and count bytes of the body
 curl -s "$1" | wc -c
