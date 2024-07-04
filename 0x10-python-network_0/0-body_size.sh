129d12de5496091cd5cb0e8f88ebd4c7c0327ca2
ranaitsan123 <95276621+ranaitsan123@users.noreply.github.com>
2024-07-04 22:58:28 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This program takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
+# Get The body  size  from outgoing  request to url introduced as argument
+curl -sI "$1" | grep 'Content-Length:' | cut -c 17-
