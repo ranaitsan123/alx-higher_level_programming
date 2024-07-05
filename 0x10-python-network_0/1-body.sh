cf7258add71409ffa115316962e8ab4c4d8daef4
ranaitsan123 <95276621+ranaitsan123@users.noreply.github.com>
2024-07-05 17:49:40 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request, follow redirects, and display body for 200 status code
-curl -sL -w "%{http_code}" "$1" | awk '/200$/ {p=1; next} p'
+curl -sL -w "%{http_code}" "$1" | awk '/200$/ {p=1} p'
+echo "Route 2"
