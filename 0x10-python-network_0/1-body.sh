c4577d07ad8f5b913eba012f6d9411d9aa48a138
ranaitsan123 <95276621+ranaitsan123@users.noreply.github.com>
2024-07-05 16:03:25 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+# Send GET request and capture the response body
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
+if [ "$response" -eq 200 ]; then
+  curl -s "$1"
+fi
