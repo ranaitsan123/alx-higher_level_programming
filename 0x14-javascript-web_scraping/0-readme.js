be7b5bf2e8f4146834b23af8b1aba635ade41be1
ranaitsan123 <95276621+ranaitsan123@users.noreply.github.com>
2024-07-31 11:23:42 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+
+// Get the file path from the command-line arguments
+const filePath = process.argv[2];
+
+// Read the file
+fs.readFile(filePath, 'utf8', (error, content) => {
+  if (error) {
+    // Print the error object if an error occurs
+    console.error(error);
+  } else {
+    // Print the file content if no error
+    console.log(content);
+  }
 });
