c51af836b1d31ad66791ad4a8edda8933f2a521c
ranaitsan123 <95276621+ranaitsan123@users.noreply.github.com>
2024-07-31 17:48:22 +0100

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,11 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2
--- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
+-- Creates the database hbtn_0d_2 if it does not exist
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+
+-- Creates the user user_0d_2 if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grants SELECT privileges on the database hbtn_0d_2 to user_0d_2
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+
+-- Applies the privilege changes
 FLUSH PRIVILEGES;
